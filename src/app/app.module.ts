6824b49173d5db675553dfefee9006e151192248
Ausan Singh <123ausan@gmail.com>
2016-11-28 19:03:51 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { AppHeader }   from './components/header/header.component';
 import { AboutSection }   from './components/aboutme/about.component';
+import { PortfolioComponent }   from './components/portfolio/portfolio.component';
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ 
     AppComponent,
     AppHeader,
-    AboutSection
+    AboutSection,
+    PortfolioComponent
    ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'portfolio',
+    template: require('./portfolio.html')
+})
+
+export class PortfolioComponent {}
